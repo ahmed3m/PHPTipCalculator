12b2d6f585f1e5cc96db1654157c3571adfeaeac
ahmed3m <ahmed_3m2010@hotmail.com>
2016-12-18 04:50:45 -0500

    Add CSS file

--- TipCalculator.php
+++ TipCalculator.php
@@ -2,13 +2,15 @@
 <html>
 	<head>
 		<title>Tip Calculator</title>
+		<link type="text/css" rel="stylesheet" href="TipCalculator.css">
 	</head>
 	<body>
-		<h2>Tip Calculator</h2>
+		<div>
+		<h2 class="centered">Tip Calculator</h2>
 		<form action="" method="POST">
 			<p>Bill Subtotal: $<input type="text" name="bill" value="<?php echo $_POST['bill']; ?>"></p>
 			<p>Tip Percentage</p>
-			<p>
+			<p class="centered">
 			<?php
 				for($i = 10; $i <= 20; $i += 5) {
 			?>
@@ -18,10 +20,14 @@
 				}
 			?>
 			</p>
-			<p><input type="submit" value="Submit"></p>
+			<p class="centered"><input type="submit" value="Submit"></p>
 		</form>
+		</div>
 	</body>
 </html>
+
+<html>
+<div id="result">
 <?php 
 	if($_POST['bill'] > 0 && isset($_POST['percent'])) {
 		$billSubtotal = $_POST['bill'];
@@ -30,5 +36,16 @@
 		$total = $billSubtotal + $tip;
 		echo "Tip: $" . $tip . '<br />';
 		echo "Total: $" . $total;
+	} elseif (!($_POST['bill'] > 0) && !(isset($_POST['percent']))) {
+		echo "Need to input proper bill subtotal value, and pick a tip percentage";
+	} elseif (!($_POST['bill'] > 0)) {
+		echo "Need to input proper bill subtotal value";
+	} else {
+		echo "Need to pick a tip percentage";
 	}
 ?>
+</div>
+</html>
+
+
+
