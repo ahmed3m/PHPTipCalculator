c89e27d0968de8b8d34fbfac8e25e62656c3866a
ahmed3m <ahmed_3m2010@hotmail.com>
2016-12-18 02:11:14 -0500

    Add radio buttons using a PHP loop.

--- TipCalculator.php
+++ TipCalculator.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
-
-<?php 
-	echo $_POST['text'];
-?>
-
-
 <html>
 	<head>
 		<title>Tip Calculator</title>
 	</head>
 	<body>
-		<h2>Tip Calculator</h1>
-		<form method="POST" action="">
+		<h2>Tip Calculator</h2>
+		<form action="" method="POST">
 			<p>Bill Subtotal: $<input type="text" name="text"></p>
 			<p>Tip Percentage</p>
-			<input type="submit" value="Submit">
+			<p>
+			<?php
+				for($i = 10; $i <= 20; $i += 5) {
+			?>
+					<input type="radio" name="percent">
+			<?php
+					echo "$i%";
+				}
+			?>
+			</p>
+			<p><input type="submit" value="Submit"></p>
 		</form>
 	</body>
 </html>
+<?php 
+	// code that will execute after submitting form
+?>
